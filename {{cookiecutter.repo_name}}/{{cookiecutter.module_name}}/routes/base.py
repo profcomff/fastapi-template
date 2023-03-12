dbb655df89d67c113a7f934327eda02ac1825635
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-03-12 13:53:06 +0300

    Fix sqlalchemy middleware

--- {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/routes/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/routes/base.py
@@ -12,8 +12,7 @@ app = FastAPI()
 app.add_middleware(
     DBSessionMiddleware,
     db_url=settings.DB_DSN,
-    session_args={"autocommit": True},
-    engine_args={"pool_pre_ping": True},
+    engine_args={"pool_pre_ping": True, "isolation_level": "AUTOCOMMIT"},
 )
 
 app.add_middleware(
