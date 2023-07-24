193964825fe11f347e78ecb49063d601c8e953bd
Stanislav Roslavtsev <jalchinn@yandex.ru>
2023-07-24 12:32:39 +0300

    ConfigDict in routes/models/base.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/routes/models/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/routes/models/base.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class Base(BaseModel):
@@ -8,5 +8,4 @@ class Base(BaseModel):
             attrs.append(f"{k}={v}")
         return "{}({})".format(self.__class__.__name__, ", ".join(attrs))
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True, extra="ignore")
